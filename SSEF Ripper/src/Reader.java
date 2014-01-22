7de14b2bc7e18043cc1b15046b25816bcc82d525
mixuelli <fash.hag@gmail.com>
2014-01-22 21:47:57 +0800

    added jars

---
+++ SSEF Ripper/src/Reader.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+* and open the template in the editor.
+ */
+import com.itextpdf.text.pdf.*;
+/**
+ *
+ * @author michelle
+ */
+public class Reader {
+
+
+}
